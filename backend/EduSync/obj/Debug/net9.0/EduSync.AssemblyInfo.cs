b7af5d3b8fa6d29201c84585b5479acc9127f4cd
lekimanh452003 <kimanh01663345754@gmail.com>
2025-05-05 11:40:12 +0700

    fix docker

--- backend/EduSync/obj/Debug/net9.0/EduSync.AssemblyInfo.cs
+++ backend/EduSync/obj/Debug/net9.0/EduSync.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduSync")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c28127b9e4a4dcc5a4c4a5113228aba1c6c44066")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e17507c250a158598ff0dbe34d4e16308e876576")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduSync")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduSync")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
